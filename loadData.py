f506e76a0dad1945a088a0753eebd55697773f27
Johannes Castner <jac2130@gmail.com>
2016-02-09 06:49:05 -0800

    fixed bugs

--- loadData.py
+++ loadData.py
@@ -13,6 +13,16 @@ with open('simpleG', 'r') as f:
 with open('complexG', 'r') as f:
     complexG = jp.decode(f.read())
 
+print simpleG
+
+
 for key in simple:
 
-    simple_score[key] = [Distance(simpleG.probs, t) for t in simple[key]]
+    simple_score[key] = [Distance(simpleG["probs"], t) for t in simple[key]]
+"""
+for key in simple:
+    print simple[key][6]
+"""
+key = simple.keys()[6]
+
+print simple_score[key]
