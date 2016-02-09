d6aa9dc5747575ae1197e6920e362041ba271c17
Johannes Castner <jac2130@gmail.com>
2016-02-08 20:23:18 -0800

    adding the simple and complex true graphical models

--- loadData.py
+++ loadData.py
@@ -1,6 +1,18 @@
 import pandas as pd 
+from Entro import Distance
+import jsonpickle as jp
 
 simple = pd.read_pickle("simple_models")
 complex = pd.read_pickle("complex_models")
 
-print simple.keys()
+simple_score = {}
+
+with open('simpleG', 'r') as f:
+    simpleG = jp.decode(f.read())
+
+with open('complexG', 'r') as f:
+    complexG = jp.decode(f.read())
+
+for key in simple:
+
+    simple_score[key] = [Distance(simpleG.probs, t) for t in simple[key]]
