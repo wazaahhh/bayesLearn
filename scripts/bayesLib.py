2d760fe39f1a3f25aac4682c121714e0eb69b752
Thomas Maillart <thomas.maillart@gmail.com>
2016-08-22 14:00:51 +0200

    added divergence versus score

--- scripts/bayesLib.py
+++ scripts/bayesLib.py
@@ -14,6 +14,25 @@ from fitting_tools import *
 
 dir = "/Users/maithoma/github/bayesLearn/scripts/"
 
+fig_width_pt = 420.0  # Get this from LaTeX using \showthe\columnwidth
+inches_per_pt = 1.0 / 72.27  # Convert pt to inch
+golden_mean = (np.sqrt(5) - 1.0) / 2.0  # Aesthetic ratio
+fig_width = fig_width_pt * inches_per_pt  # width in inches
+fig_height = fig_width  # *golden_mean      # height in inches
+fig_size = [fig_width, fig_height]
+
+params = {'backend': 'ps',
+          'axes.labelsize': 25,
+          'text.fontsize': 32,
+          'legend.fontsize': 18,
+          #'title.fontsize': 20,
+          'xtick.labelsize': 20,
+          'ytick.labelsize': 20,
+          'text.usetex': False,
+          'figure.figsize': fig_size}
+pl.rcParams.update(params)
+
+
 
 def rankorder(x):
 	x1 = list(np.sort(x))
