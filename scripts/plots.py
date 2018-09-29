0cd3b82daaf3a6b9999f63cacaa8c7d86dcbc67e
Johannes Castner <jac2130@gmail.com>
2018-09-29 18:31:23 +0100

    Update plots.py

--- scripts/plots.py
+++ scripts/plots.py
@@ -1,56 +1,55 @@
-import matplotlib.pyplot as pl
-import numpy as np
-import pandas as pd
-import jsonpickle as jp
-from Entro import Distance
-simple = pd.read_pickle("simple_models")
-complex = pd.read_pickle("complex_models")
+        pl.xlim(0,2500)
+        pl.legend(loc=0)
+        pl.savefig(name+'.eps')
 
-simple_score = {}
-complex_score = {}
+distance_plot(simple_distances, complex_distances)
 
-with open('simpleG', 'r') as f:
-    simpleG = jp.decode(f.read())
 
-with open('complexG', 'r') as f:
-    complexG = jp.decode(f.read())
-
-for key in simple:
-    simple_score[key] = [Distance(simpleG["probs"], t) for t in simple[key]]
-
-for key in complex:
-    complex_score[key] = [Distance(complexG["probs"], t) for t in complex[key]]
-
-simple_distances = pd.DataFrame(simple_score)
-complex_distances = pd.DataFrame(complex_score)
-
-color = ["green","red"]
-legend = ["simple","complex"]
-
-pl.close()
-for k,kx  in enumerate([simple_distances,complex_distances]):
-    #array = np.array(dfSimpleLearning)
-    ar = np.array(kx)
-    mean = []
-    median = []
-    std = []
-
-    for i,ix in enumerate(ar):
-        cond = np.logical_not(np.isnan(ix))
-        median = np.append(median,np.median(ix[cond]))
-        mean  = np.append(mean,np.mean(ix[cond]))
-        std = np.append(std,np.std(ix[cond]))
-
-        #pl.plot(median)
-    pl.figure(1)
-    pl.plot(mean,color=color[k],label=legend[k])
-    pl.plot(mean+std,'--',color=color[k])
-    pl.plot(mean-std,'--',color=color[k])
-
-#pl.figure(2)
-#plotFit(mean)
-
-pl.xlim(0,2500)
-pl.legend(loc=0)
-pl.savefig('foo.eps')
 #pl.show()
+import string
+#ps = list(zip(string.ascii_lowercase, simpleG['probs']))
+
+from scipy import random
+
+
+from numpy import zeros
+
+simpleProbs     =simpleG['probs']
+complexProbs    =complexG['probs']
+#fmereqPrior[-2] = 2
+
+#print(Distance(simpleProbs, freqPrior))
+
+
+
+def rational_learning(simpleProbs, complexProbs,time_sample = 2500, sample =1000, sd={}, cd={}):
+    for j in range(sample):
+        simplePrior = list(zeros(len(simpleProbs)))
+        complexPrior =list(zeros(len(complexProbs)))
+        simple_distances = list(zeros(time_sample))
+        complex_distances= list(zeros(time_sample))
+        for t in range(time_sample):
+            simple_rand =random.random()
+            complex_rand = random.random()
+            simple_cum =0
+            complex_cum=0
+            for i in range(len(simpleProbs)):
+                simple_cum+=simpleProbs[i]
+                if simple_cum>simple_rand:
+                    simplePrior[i]+=1
+                    simple_distances[t] =Distance(simplePrior, simpleProbs)
+                    break
+            for i in range(len(complexProbs)):
+                complex_cum+=complexProbs[i]
+                if complex_cum>complex_rand:
+                    complexPrior[i]+=1
+                    complex_distances[t] = Distance(complexPrior, complexProbs)
+                    break
+
+        sd[j]=simple_distances
+        cd[j]=complex_distances
+    return pd.DataFrame(sd), pd.DataFrame(cd)
+
+simple_rational, complex_rational = rational_learning(simpleProbs, complexProbs)
+distance_plot(simple_rational, complex_rational, name="rational")
+"plots.py" 109L, 3198C                                                                                                                                                                    106,5         Bot
